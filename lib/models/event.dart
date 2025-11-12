bb8e0e2870a30e5728252c89bb6a5c095203992f
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-11-12 15:45:12 +0700

    feat: add resident home screen

---
+++ lib/models/event.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Event {
+  final String category;
+  final String title;
+  final String location;
+  final String date;
+  final String time;
+  final Color categoryColor;
+
+  Event({
+    required this.category,
+    required this.title,
+    required this.location,
+    required this.date,
+    required this.time,
+    required this.categoryColor,
+  });
+}
