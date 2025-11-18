0bfa764e38da3d7a25c5463b975196d000f3c56f
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-11-18 20:14:53 +0700

    refactor: remove old version of `rt/rw` pages screen

--- lib/pages/block_leader/laporan/models/rt_report.dart
+++
@@ -1,8 +0,0 @@
-// lib/laporan/models/rt_report.dart
-class RTReport {
-  final String rt;
-  final int total;
-  final int selesai;
-
-  RTReport({required this.rt, required this.total, required this.selesai});
-}
