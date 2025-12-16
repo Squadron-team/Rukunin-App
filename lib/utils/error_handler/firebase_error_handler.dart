927249a421c174a5b75014e5e945d81fe6d30423
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-12-16 15:32:36 +0700

    refactor: move all error handler utilities to `lib/utils/error_handler`

--- lib/utils/error_handler/firebase_error_handler.dart
+++ lib/utils/error_handler/firebase_error_handler.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class FirebaseAuthHelper {
+class FirebaseErrorHandler {
   static String translateErrorMessage(FirebaseAuthException error) {
     String errorMessage;
 
